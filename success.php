d913f74236e8acdc401488aba0ce23350e481197
OlegMishhenko <flashdwyanewade@yandex.ru>
2020-04-09 15:31:55 +0700

    Установка метрики

--- success.php
+++ success.php
@@ -10,6 +10,25 @@
 	<title>Заказ принят</title>
 	<link rel="stylesheet" href="css/style.css">
 	<meta http-equiv="Refresh" content="5; URL=/">
+	<!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+        (function (m, e, t, r, i, k, a) {
+        m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) };
+            m[i].l = 1 * new Date(); k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+        })
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(54368968, "init", {
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true,
+            webvisor: true
+        });
+    </script>
+    <noscript>
+        <div><img src="https://mc.yandex.ru/watch/54368968" style="position:absolute; left:-9999px;" alt="" /></div>
+    </noscript>
+    <!-- /Yandex.Metrika counter -->
 </head>
 <body>
 	<header class="thank-you-page">
